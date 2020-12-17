fc96fbfa99328338881f8ac70b1d38b9472bd642
Nora El Naby <nelnaby@zagmail.gonzaga.edu>
2020-12-17 15:48:01 -0800

    "Starting to create fragments for Main Activity (SQLQuery and Attribute)"

--- app/src/main/java/com/example/theatercrowd/SQLQuery.java
+++ app/src/main/java/com/example/theatercrowd/SQLQuery.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,14 +20,7 @@ import android.view.ViewGroup;
  */
 public class SQLQuery extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Spinner objectType;
 
     public SQLQuery() {
         // Required empty public constructor
@@ -32,8 +30,6 @@ public class SQLQuery extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment SQLQuery.
      */
     // TODO: Rename and change types and number of parameters
@@ -45,10 +41,9 @@ public class SQLQuery extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        objectType = getActivity().findViewById(R.id.spinner_object);
+        String[] objectTypeValues = new String[]{"Person", "Movie", "Award"};
+        setSpinnerValues(objectType, objectTypeValues );
     }
 
     @Override
@@ -56,5 +51,9 @@ public class SQLQuery extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_s_q_l_query, container, false);
+    }
+
+    private void setSpinnerValues(Spinner spinner, String[] values) {
+        spinner.setAdapter(new ArrayAdapter(getContext(), R.layout.support_simple_spinner_dropdown_item, values));
     }
 }

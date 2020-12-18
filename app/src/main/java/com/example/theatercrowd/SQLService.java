af6b926637ed7906e162e16f3aace1fce7bb8dfd
cffrench <connor.ffrench@san.rr.com>
2020-12-18 11:15:17 -0800

    Select Function is closer to working

--- app/src/main/java/com/example/theatercrowd/SQLService.java
+++ app/src/main/java/com/example/theatercrowd/SQLService.java
@@ -4,13 +4,18 @@ import android.app.Service;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import static java.lang.Boolean.TRUE;
 
 public class SQLService extends Service {
 
@@ -72,25 +77,26 @@ public class SQLService extends Service {
 
 
     private static final String SQL_CREATE_TABLE_PEOPLE =
-            "CREATE TABLE People ( Name Text, Birthdate Date, Bio Text NOT NULL, Nationality Text, PRIMARY KEY (Name, Birthdate))";
+            "CREATE TABLE IF NOT EXISTS People ( Name Text, Birthdate Date, Bio Text NOT NULL, Nationality Text, PRIMARY KEY (Name, Birthdate))";
 
     private static final String SQL_CREATE_TABLE_MOVIES =
-            "CREATE TABLE Movies ( Title Text, ReleaseYear Smallint, Genre Text, Description Varchar (250), PRIMARY KEY (Title, ReleaseYear))";
+            "CREATE TABLE IF NOT EXISTS Movies ( Title Text, ReleaseYear Smallint, Genre Text, Description Varchar (250), PRIMARY KEY (Title, ReleaseYear))";
 
     private static final String SQL_CREATE_TABLE_MOVIEREVIEWS =
-            "CREATE TABLE MovieReviews ( ReviewNumber Smallint, PosterNickname Varchar(30) NOT NULL, MovieScore DECIMAL(1,2)  NOT NULL CHECK(MovieScore <= 10), Review Varchar (300) NOT NULL, PRIMARY KEY (ReviewNumber))";
+            "CREATE TABLE IF NOT EXISTS MovieReviews ( ReviewNumber Smallint, PosterNickname Varchar(30) NOT NULL, MovieScore DECIMAL(1,2)  NOT NULL CHECK(MovieScore <= 10), Review Varchar (300) NOT NULL, PRIMARY KEY (ReviewNumber))";
 
     private static final String SQL_CREATE_TABLE_AWARDS =
-            "CREATE TABLE Awards ( AwardName Text, AwardYear Smallint, MovieTitle Text NOT NULL, MovieReleaseYear Smallint NOT NULL, AwardWinner Text, AwardWinnerBirthDate Date, PRIMARY KEY (AwardName, AwardYear), FOREIGN KEY (MovieTitle, MovieReleaseYear) REFERENCES Movies (Title, ReleaseYear), FOREIGN KEY (AwardWinner, AwardWinnerBirthDate) REFERENCES People (Name, Birthdate),CHECK (AwardYear >= MovieReleaseYear))";
+            "CREATE TABLE IF NOT EXISTS Awards ( AwardName Text, AwardYear Smallint, MovieTitle Text NOT NULL, MovieReleaseYear Smallint NOT NULL, AwardWinner Text, AwardWinnerBirthDate Date, PRIMARY KEY (AwardName, AwardYear), FOREIGN KEY (MovieTitle, MovieReleaseYear) REFERENCES Movies (Title, ReleaseYear), FOREIGN KEY (AwardWinner, AwardWinnerBirthDate) REFERENCES People (Name, Birthdate),CHECK (AwardYear >= MovieReleaseYear))";
 
     private static final String SQL_CREATE_TABLE_REVIEWS =
-            "CREATE TABLE Reviews( ReviewNumber Smallint, Title Text, ReleaseYear Smallint, PRIMARY KEY (ReviewNumber, Title, ReleaseYear), FOREIGN KEY (ReviewNumber) REFERENCES MovieReviews(ReviewNumber), FOREIGN KEY (Title, ReleaseYear) REFERENCES Movies (Title, ReleaseYear))";
+            "CREATE TABLE IF NOT EXISTS Reviews( ReviewNumber Smallint, Title Text, ReleaseYear Smallint, PRIMARY KEY (ReviewNumber, Title, ReleaseYear), FOREIGN KEY (ReviewNumber) REFERENCES MovieReviews(ReviewNumber), FOREIGN KEY (Title, ReleaseYear) REFERENCES Movies (Title, ReleaseYear))";
 
     private static final String SQL_CREATE_TABLE_MOVIECREDITS =
-            "CREATE TABLE MovieCredits ( Name Text, Birthdate Date, MovieTitle Text, MovieReleaseYear Smallint, Role Text, PRIMARY KEY (Name, Birthdate, MovieTitle, MovieReleaseYear, Role), FOREIGN KEY (Name, Birthdate) REFERENCES People (Name, Birthdate), FOREIGN KEY (MovieTitle, MovieReleaseYear) REFERENCES Movies (Title, ReleaseYear))";
+            "CREATE TABLE IF NOT EXISTS MovieCredits ( Name Text, Birthdate Date, MovieTitle Text, MovieReleaseYear Smallint, Role Text, PRIMARY KEY (Name, Birthdate, MovieTitle, MovieReleaseYear, Role), FOREIGN KEY (Name, Birthdate) REFERENCES People (Name, Birthdate), FOREIGN KEY (MovieTitle, MovieReleaseYear) REFERENCES Movies (Title, ReleaseYear))";
 
 
     public void insertPeople(String name, Date birthdate, String bio, String nationality) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(PEOPLE_NAME, name);
         values.put(PEOPLE_BIRTHDATE, birthdate.toString());
@@ -102,6 +108,7 @@ public class SQLService extends Service {
     }
 
     public void insertMovies(String title, int releaseYear, String genre, String description) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(MOVIES_TITLE, title);
         values.put(MOVIES_RELEASEYEAR, releaseYear);
@@ -114,6 +121,7 @@ public class SQLService extends Service {
 
 
     public void insertReviews(String title, int releaseYear, String posterNickname, float movieScore, String review) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(REVIEWS_TITLE, title);
         values.put(REVIEWS_RELEASEYEAR, releaseYear);
@@ -124,6 +132,7 @@ public class SQLService extends Service {
     }
 
     public void insertMovieReviews(String posterNickname, float movieScore, String review) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(MOVIEREVIEWS_POSTERNICKNAME, posterNickname);
         values.put(MOVIEREVIEWS_MOVIESCORE, movieScore);
@@ -134,6 +143,7 @@ public class SQLService extends Service {
     }
 
     public void insertAwards(String awardName, int awardYear, String movieTitle, int movieReleaseYear, String awardWinner, Date awardWinnerBirthdate) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(AWARDS_AWARDNAME, awardName);
         values.put(AWARDS_AWARDYEAR, awardYear);
@@ -147,6 +157,7 @@ public class SQLService extends Service {
     }
 
     public void insertMovieCredits(String name, Date birthdate, String movieTitle, int movieReleaseYear, String role) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(MOVIECREDITS_NAME, name);
         values.put(MOVIECREDITS_BIRTHDATE, birthdate.toString());
@@ -156,6 +167,56 @@ public class SQLService extends Service {
 
         // Insert the new row, returning the primary key value of the new row
         long newRowId = db.insert(MOVIECREDITS, null, values);
+    }
+
+    public void updateRow(String tableName, String[] columnsToUpdate, String[] valuesToUpdate) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String title = "MyNewTitle";
+        ContentValues values = new ContentValues();
+        values.put(FeedEntry.COLUMN_NAME_TITLE, title);
+
+        // Which row to update, based on the title
+        String selection = FeedEntry.COLUMN_NAME_TITLE + " LIKE ?";
+        String[] selectionArgs = { "MyOldTitle" };
+
+        int count = db.update(
+                tableName,
+                values,
+                selection,
+                selectionArgs);
+    }
+
+    public void selectRows(String[] selectQuery) {
+        String queiredTables = tableNames[0];
+            for(int i=1; i<tableNames.length; i++) {
+                   queiredTables += ", " + tableNames[i];
+            }
+        String sortOrder = "";
+            if(sortAsc == TRUE) {
+                sortOrder = sortAttribute + " ASC";
+            }
+            else {
+                sortOrder = sortAttribute + " DESC";
+            }
+
+        Cursor cursor = db.query(
+                queiredTables,                  // The tables to query
+                requestedColumns,               // The array of columns to return (pass null to get all)
+                selectionColumn,                // The columns for the WHERE clause
+                selectionArguments,             // The values for the WHERE clause
+                groupBy,                        // how to group the rows group the rows
+                having,                         // how to filter by row groups
+                sortOrder                       // What you are sorting on + " ASC" or " DESC"
+        );
+
+        List itemIds = new ArrayList<>();
+        while(cursor.moveToNext()) {
+            long itemId = cursor.getLong(
+                    cursor.getColumnIndexOrThrow());
+                    cursor.getString(itemId);
+            itemIds.add(itemId);
+        }
+        cursor.close();
     }
 
 }
